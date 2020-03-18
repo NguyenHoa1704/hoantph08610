efb1ac82af0c2725eedc87890cea59014da71f52
NguyenHoa1704 <nguyenthihoa04022000@gmail.com>
2020-03-18 22:55:33 +0700

    2 files modified, 1 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../Product';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
-
+//  name: string = 'Item 1';
+product : Product = {
+  name: 'nguyễn thị hoa',
+  price: 100000000,
+  desc: 'xinh dep',
+  img: 'https://dummyimage.com/275x175/000/fff',
+  status: false
+}
+changeStatus(){
+  this.product.status= !this.product.status;
+}
+changeName(e){
+  this.product.name= e.target.value;
+}
 }
