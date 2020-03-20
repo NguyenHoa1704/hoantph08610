6f9aaaf2836342470e92462b0a0b594d917b743d
NguyenHoa1704 <nguyenthihoa04022000@gmail.com>
2020-03-20 18:41:46 +0700

    1 file modified

--- src/app/MockData.ts
+++ src/app/MockData.ts
@@ -10,18 +10,18 @@ export const data : Product[] = [
     },
     {
       id: 2,
-      name: "Item 2",
-      price: 2250,
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+      name: "8.3.2019",
+      price: 10000000000000,
+      desc: 'Đi chơi :v',
       status: false,
-      img: 'https://dummyimage.com/275x175/000/fff'
+      img: 'https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-9/p720x720/53622413_2199234137007077_7401231362662858752_o.jpg?_nc_cat=111&_nc_sid=110474&_nc_ohc=AfbW7X7WrrYAX9Nrq6Q&_nc_ht=scontent-hkt1-1.xx&_nc_tp=6&oh=76612bab207b117c9e23b3e4b2a0dc29&oe=5E9A2554'
     },
     {
       id: 3,
-      name: "Item 3",
-      price: 220,
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+      name: "Hoa & Nhung",
+      price: 0,
+      desc: 'Chúng mình TỪNG là bạn thân :) ',
       status: false,
-      img: 'https://dummyimage.com/275x175/000/fff'
+      img: 'https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-9/p720x720/54369341_2199234100340414_6543748228712497152_o.jpg?_nc_cat=110&_nc_sid=110474&_nc_ohc=CaNsWFT0P-0AX-eADUm&_nc_ht=scontent-hkt1-1.xx&_nc_tp=6&oh=8a961c557d6bac12da3b76d95f72fa42&oe=5E9B940B'
     }
 ]
