4946428db88c32ab339f91b8adf0ae232bd3e544
NguyenHoa1704 <nguyenthihoa04022000@gmail.com>
2020-03-20 19:14:31 +0700

    1 file modified

--- src/app/MockData.ts
+++ src/app/MockData.ts
@@ -23,5 +23,61 @@ export const data : Product[] = [
       desc: 'Chúng mình TỪNG là bạn thân :) ',
       status: false,
       img: 'https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-9/p720x720/54369341_2199234100340414_6543748228712497152_o.jpg?_nc_cat=110&_nc_sid=110474&_nc_ohc=CaNsWFT0P-0AX-eADUm&_nc_ht=scontent-hkt1-1.xx&_nc_tp=6&oh=8a961c557d6bac12da3b76d95f72fa42&oe=5E9B940B'
+    },
+    {
+      id: 4,
+      name: "Hoa & Nhung",
+      price: 10000000000000,
+      desc: 'Kỷ niệm của chúng mình',
+      status: false,
+      img: 'https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-9/s960x960/53336767_2199234063673751_1524021651237240832_o.jpg?_nc_cat=103&_nc_sid=110474&_nc_ohc=aznuppP-Yc8AX_CAh7Y&_nc_ht=scontent-hkt1-1.xx&_nc_tp=7&oh=ead344e7f54f2e2e48f56c6adc91cce7&oe=5E99004B'
+    },
+    {
+      id: 5,
+      name: "Đi chơi",
+      price: 0,
+      desc: 'bún đậu mẹt với bún cá :v ',
+      status: false,
+      img: 'https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-9/p960x960/53289850_2199234033673754_325001876095369216_o.jpg?_nc_cat=103&_nc_sid=110474&_nc_ohc=Gg0RVvI7qzQAX8dfoOa&_nc_ht=scontent-hkt1-1.xx&_nc_tp=6&oh=ae3d8fbfbbeab8d7853f85aede811401&oe=5E9B47E1'
+    },
+    {
+      id: 6,
+      name: "Hoa & Nhung",
+      price: 0,
+      desc: 'Chúng mình TỪNG là bạn thân :) ',
+      status: false,
+      img: 'https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-9/p720x720/54369341_2199234100340414_6543748228712497152_o.jpg?_nc_cat=110&_nc_sid=110474&_nc_ohc=CaNsWFT0P-0AX-eADUm&_nc_ht=scontent-hkt1-1.xx&_nc_tp=6&oh=8a961c557d6bac12da3b76d95f72fa42&oe=5E9B940B'
+    },
+    {
+      id: 7,
+      name: "Hoa & Nhung",
+      price: 0,
+      desc: 'Chúng mình TỪNG là bạn thân :) ',
+      status: false,
+      img: 'https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-9/p720x720/54369341_2199234100340414_6543748228712497152_o.jpg?_nc_cat=110&_nc_sid=110474&_nc_ohc=CaNsWFT0P-0AX-eADUm&_nc_ht=scontent-hkt1-1.xx&_nc_tp=6&oh=8a961c557d6bac12da3b76d95f72fa42&oe=5E9B940B'
+    },
+    {
+      id: 8,
+      name: "Hoa & Nhung",
+      price: 0,
+      desc: 'Bánh sinh nhật nhung ',
+      status: false,
+      img: 'https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-9/p720x720/45565119_2122688247995000_9221568623036858368_o.jpg?_nc_cat=104&_nc_sid=0be424&_nc_ohc=iqaHgEnogScAX-YZjAU&_nc_ht=scontent-hkt1-1.xx&_nc_tp=6&oh=e1fc34b1b609cd6a7cc7bb13bd3edd75&oe=5E9C2321'
+    },
+    {
+      id: 9,
+      name: "Hoa & Nhung",
+      price: 100000000000000,
+      desc: 'Sinh nhật nhung ',
+      status: false,
+      img: 'https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-9/p720x720/45578351_2122688807994944_6562038613230485504_o.jpg?_nc_cat=101&_nc_sid=0be424&_nc_ohc=oX87jVtUYvwAX_5miOJ&_nc_ht=scontent-hkt1-1.xx&_nc_tp=6&oh=c9dd2ec138c94c6460477f5cde31b310&oe=5E9C2458'
+    },
+    {
+      id: 10,
+      name: "Hoa & Nhung",
+      price: 0,
+      desc: 'Chúng mình TỪNG là bạn thân :) ',
+      status: false,
+      img: 'https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-9/p720x720/54369341_2199234100340414_6543748228712497152_o.jpg?_nc_cat=110&_nc_sid=110474&_nc_ohc=CaNsWFT0P-0AX-eADUm&_nc_ht=scontent-hkt1-1.xx&_nc_tp=6&oh=8a961c557d6bac12da3b76d95f72fa42&oe=5E9B940B'
     }
 ]
